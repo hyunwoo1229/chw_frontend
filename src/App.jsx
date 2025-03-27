1e5b70554911d8900a12bae65cbd3267905b6ae4
unknown <hynoow20011229@gmail.com>
2025-03-28 00:00:14 +0900

    fisrt commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
+}
+
+export default App;
